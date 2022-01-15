a7c8423159684409118af2bffdb0b3a53fa5780e
Robin Burri <robinburri@tutanota.com>
2022-01-15 15:41:40 +0100

    gnl success

---
+++ rank02/samedi/gnl/get_next_line.h
@@ -0,0 +1,23 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct s_info
+{
+	int fd;
+	int pos;
+	int hasread;
+	char buf[BUFFER_SIZE];
+}			t_info;
+
+char check_read(t_info *info);
+int check_init(int fd, t_info *info);
+char	*get_next_line(int fd);
+
+#endif
